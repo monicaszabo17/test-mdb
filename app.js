16f6669563ca54da04dfd06db3c5d21ba035b667
MonicaSzabo <dragontree17@gmail.com>
2021-02-01 11:20:07 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var express = require("express");
+const bodyParser = require("body-parser");
+
+
+var todoRoutes = require("./routes/todoRoutes");
+var app = express();
+var mongoose = require("mongoose");
+var devurl = "MDB ATLAS URL HERE";
+
+var mongodb = process.env.MONGODB_URI || devurl ;
+
+
+mongoose.connect(mongodb, {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MDB connection Error :"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use("/todos", todoRoutes);
+
+var port = 3000;
+app.listen(port, () => {
+    console.log(`Server run on port number" + ${port}`);
+});
+
